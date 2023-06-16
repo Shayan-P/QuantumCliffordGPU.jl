a59ab3088d74a87cca8a68a8742683881702ccca
Shayan-P <shayanpardis82@gmail.com>
2023-06-16 11:31:45 -0400

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,19 @@
 using QuantumClifford
-using QuantumCliffordGPU
 using CUDA
+using QuantumCliffordGPU
 using Test
 
 @testset "QuantumCliffordGPU.jl" begin
     @test begin
         p = P"_IZXY"
-        typeof(p.xz) <: CUDA.CuArray # this is a bad test because it depends on representation of data in QuantumClifford. Change later...
+        p_gpu = to_gpu(p)
+        typeof(p_gpu.xz) <: CUDA.CuArray # this is a bad test because it depends on representation of data in QuantumClifford. Change later...
     end
+    @test begin
+        s = S"-XX
+              +ZZ"
+        s_gpu = to_gpu(s)
+        typeof(tab(s_gpu).xzs) <: CUDA.CuArray # this is a bad test because it depends on representation of data in QuantumClifford. Change later...
+    end
+
 end
