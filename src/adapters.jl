7ce1c640f557e5d37cb4ee2ebb4b16e2ca8c9882
Shayan-P <shayanpardis82@gmail.com>
2023-06-28 10:02:55 -0400

    add adapters for PauliFrames

--- src/adapters.jl
+++ src/adapters.jl
@@ -1,25 +1,33 @@
+to_gpu(array::AbstractArray) = CuArray(array);
+
+to_cpu(array::CuArray{T, 0}) where{T} = Array(array);
+
+to_cpu(array::CuArray{T, 1}) where{T} = Array(array);
+
+to_cpu(array::CuArray{T, 2}) where{T} = Matrix(array);
+
 # maybe change the format of storing the data in gpu array 
 # so that it is more convinient to work with them on gpu?
-function to_gpu(tab::QuantumClifford.Tableau)
-    QuantumClifford.Tableau(CuArray(tab.phases), tab.nqubits, CuArray(tab.xzs))
-end
+to_gpu(tab::QuantumClifford.Tableau) =
+    QuantumClifford.Tableau(to_gpu(tab.phases), tab.nqubits, to_gpu(tab.xzs))
 
-function to_cpu(tab::QuantumClifford.Tableau)
-    QuantumClifford.Tableau(Array(tab.phases), tab.nqubits, Matrix(tab.xzs))
-end
+to_cpu(tab::QuantumClifford.Tableau) =
+    QuantumClifford.Tableau(to_cpu(tab.phases), tab.nqubits, to_cpu(tab.xzs))
 
-function to_gpu(pauli::QuantumClifford.PauliOperator) 
-    QuantumClifford.PauliOperator(CuArray(pauli.phase), pauli.nqubits, CuArray(pauli.xz))
-end
+to_gpu(pauli::QuantumClifford.PauliOperator) =
+    QuantumClifford.PauliOperator(to_gpu(pauli.phase), pauli.nqubits, to_gpu(pauli.xz))
 
-function to_cpu(pauli::QuantumClifford.PauliOperator)
-    QuantumClifford.PauliOperator(Array(pauli.phase), pauli.nqubits, Array(pauli.xz))
-end
+to_cpu(pauli::QuantumClifford.PauliOperator) =
+    QuantumClifford.PauliOperator(to_cpu(pauli.phase), pauli.nqubits, to_cpu(pauli.xz))
 
-function to_gpu(stabilizer::QuantumClifford.Stabilizer) 
+to_gpu(stabilizer::QuantumClifford.Stabilizer) =
     Stabilizer(to_gpu(tab(stabilizer)))
-end
 
-function to_cpu(stabilizer::QuantumClifford.Stabilizer) 
+to_cpu(stabilizer::QuantumClifford.Stabilizer) = 
     Stabilizer(to_cpu(tab(stabilizer)))
-end
+
+to_gpu(pauli_frame::QuantumClifford.PauliFrame) =
+    PauliFrame(to_gpu(pauli_frame.frame), to_gpu(pauli_frame.measurements))
+
+to_cpu(pauli_frame::QuantumClifford.PauliFrame) =
+    PauliFrame(to_cpu(pauli_frame.frame), to_cpu(pauli_frame.measurements))
